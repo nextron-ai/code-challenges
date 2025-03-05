1451b627b11d3f3dcc191093d9bae60a356f1464
MarcusVLMA <marcusviniciuslma@gmail.com>
2025-03-05 18:14:35 -0300

    data/data.go: adding more data

--- golang/data/data.go
+++ golang/data/data.go
@@ -1,40 +1,60 @@
 package data
 
+// Usina 1
+var PowerPlant1 = map[string]any{
+	"id":   1,
+	"name": "Power Plant 1",
+}
+
 // Dados de medição da usina 1
 var MeasurementsPowerPlant1 = []map[string]any{
 	{
+		"id":            1,
 		"energy_in_kwh": 20,
 		"date_time":     "2022-01-01 10:20:00",
 	},
 	{
+		"id":            2,
 		"energy_in_kwh": 40,
 		"date_time":     "2022-01-01 10:30:00",
 	},
 	{
+		"id":            3,
 		"energy_in_kwh": 30,
 		"date_time":     "2022-01-01 10:40:00",
 	},
 	{
+		"id":            4,
 		"energy_in_kwh": 20,
 		"date_time":     "2022-01-01 10:50:00",
 	},
 }
 
+// Usina 2
+var PowerPlant2 = map[string]any{
+	"id":   2,
+	"name": "Power Plant 2",
+}
+
 // Dados de medição da usina 2
 var MeasurementsPowerPlant2 = []map[string]any{
 	{
+		"id":            5,
 		"energy_in_kwh": 10,
 		"date_time":     "2022-01-01 10:20:00",
 	},
 	{
+		"id":            6,
 		"energy_in_kwh": 20,
 		"date_time":     "2022-01-01 10:30:00",
 	},
 	{
+		"id":            7,
 		"energy_in_kwh": 15,
 		"date_time":     "2022-01-01 10:40:00",
 	},
 	{
+		"id":            8,
 		"energy_in_kwh": 5,
 		"date_time":     "2022-01-01 10:50:00",
 	},
